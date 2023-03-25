fc117f7e14a845af2a7678f501890f83a88d7351
Nanda-Tamala <nandasari437@gmail.com>
2023-03-25 14:22:47 +0700

    pustaka booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Nanda Tamala Sari
+            Saya tinggal di sekitar bekasi barat
+            hobbi saya adalah rebahan";
+    }
+}
